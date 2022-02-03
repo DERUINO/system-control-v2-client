f6589f36733549b669cf3da9c898a249d08e850b
Kirill Khromov <k.hromov@devspark.ru>
2022-02-03 20:14:21 +0300

    wip chat

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -9,26 +9,35 @@
         <div v-show="!dialog.recieveId" class="chat-none-selected"><div>выберите диалог</div></div>
         <div v-show="dialog.recieveId" class="chat-messages-wrapper">
             <div class="chat-messages-profile">{{ dialog.name }}</div>
-            <div class="chat-messages-container" ref="messages">
-                <div class="chat-messages" id="messages">
-                    <div v-for="(message, messageIndex) in messages" :key="messageIndex" class="chat-message">
-                        <div class="message-name">{{ message.authorId.username }}</div>
-                        <div class="message-text">{{ message.text }}</div>
+            <div class="flexbox">
+                <div class="chat-messages-container">
+                    <div class="chat-messages" ref="messages">
+                        <div
+                        v-for="(message, messageIndex) in preparedMessages"
+                        :key="messageIndex"
+                        class="chat-message"
+                        :class="{ 'unreaded': message.unread, 'none-concated': !message.concated }">
+                            <div class="message-header" v-if="!message.concated">
+                                <div class="message-name">{{ message.authorId.username }}</div>
+                                <div class="message-date">{{ convertDate(message.createdAt) }}</div>
+                            </div>
+                            <div class="message-text">{{ message.text }}</div>
+                        </div>
+                        <div class="chat-messages-bottom"></div>
                     </div>
                 </div>
-                <div class="chat-messages-bottom"></div>
-            </div>
-            <div
-                v-show="isNewMessagesButtonVisible"
-                class="chat-messages-unread"
-                @click="lastMessageScroll"
-            >
-                <img src="@/assets/arrow-down.svg">
-                <span>{{ unreadMessagesCount }}</span>
-            </div>
-            <div class="chat-send">
-                <input type="text" v-model="message.current" @keyup.enter="sendMessage">
-                <button type="button" @click="sendMessage">отправить</button>
+                <div
+                    v-show="isNewMessagesButtonVisible"
+                    class="chat-messages-unread"
+                    @click="lastMessageScroll"
+                >
+                    <img src="@/assets/arrow-down.svg">
+                    <span>{{ unreadMessagesCount }}</span>
+                </div>
+                <div class="chat-send">
+                    <input type="text" v-model="message.current" @keyup.enter="sendMessage">
+                    <button type="button" @click="sendMessage">отправить</button>
+                </div>
             </div>
         </div>
     </div>
@@ -67,17 +76,30 @@ export default {
             return this.accounts.filter(user => user._id !== this.userInfo._id);
         },
 
-        preparedMessages() {
-            return this.messages.reduce((prevent, current, index, array) => {
-                return prevent.authorId?._id === this.userInfo._id
-                    ? prevent + 1
-                    : 0;
-            }, 0)
-        },
-
         isNewMessagesButtonVisible() {
             return !this.allMessagesReaded && !this.isScrollEnded;
         },
+
+        preparedDate() {
+            return new Date(this.userInfo.createdAt) - 10000;
+        },
+
+        preparedMessages() {
+            let obj = {};
+
+            return this.messages.map(item => {
+                if (item.authorId._id === obj.authorId?._id) {
+                    return {
+                        ...item,
+                        concated: true,
+                    }
+                }
+
+                obj = item;
+
+                return item;
+            })
+        }
     },
     async created() {
         await this.getAccounts();
@@ -101,7 +123,8 @@ export default {
                         username: this.userInfo.username
                     },
                     recieveId: this.dialog.recieveId,
-                    text: this.message.current
+                    text: this.message.current,
+                    createdAt: Date.now(),
                 }
 
                 this.message.current = '';
@@ -180,6 +203,10 @@ export default {
                     
                 })
             });
+        },
+
+        convertDate(date) {
+            return new Date(date).toLocaleString('ru', { day: 'numeric', month: 'numeric', year: '2-digit', hour: 'numeric', minute: 'numeric' });
         }
     }
 }
@@ -238,89 +265,114 @@ export default {
         }
     }
 
-    .chat-messages {
-        margin-top: 60px;
-    }
-
-    .chat-messages-profile {
-        position: absolute;
-        top: 0;
-        left: 0;
-        background: white;
-        width: 100%;
-        padding: 15px;
-        border-bottom: 1px solid gray;
-    }
-
-    .chat-messages-unread {
-        position: absolute;
-        bottom: 60px;
-        right: 50px;
-        width: 42px;
-        height: 42px;
-        box-shadow: 0 0 3px rgba(0,0,0, 0.5);
-        line-height: 1;
-        padding-top: 10px;
-        text-align: center;
-        font-weight: bolder;
-        border-radius: 50%;
-        background: white;
-        cursor: pointer;
-
-        &:hover {
-            background: rgba(0,0,0,0.05);
-        }
-
-        img {
-            margin-top: 2px;
-            width: 18px;
-        }
-
-        span {
-            position: absolute;
-            top: -5px;
-            right: -5px;
-            background: gray;
-            padding: 5px;
-            font-size: 12px;
-            color: white;
-            line-height: 0.8;
-            margin-left: 1px;
-            font-weight: normal;
-            width: 18px;
-            height: 18px;
-            border-radius: 50%;
-        }
-    }
-
     .chat-messages-wrapper {
         position: relative;
         width: 100%;
-        height: 500px;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
-    }
 
-    .chat-messages-container {
-        padding: 0 30px;
-        max-height: 100%;
-        overflow: auto;
-    }
-
-    .chat-message {
-        width: 100%;
-        padding-bottom: 10px;
-
-        .message-name {
-            font-weight: bolder;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            max-width: 150px;
+        .chat-messages-profile {
+            background: white;
+            width: 100%;
+            padding: 15px;
+            border-bottom: 1px solid gray;
         }
 
-        .message-text {
-            font-size: 14px;
+        .flexbox {
+            height: 100%;
+
+            .chat-messages-container {
+                position: relative;
+                width: 100%;
+                height: 414px;
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-end;
+
+                .chat-messages {
+                    max-height: 100%;
+                    overflow: auto;
+                    display: flex;
+                    flex-direction: column;
+
+                    .chat-message {
+                        width: 100%;
+                        padding: 0 15px;
+
+                        &.unreaded {
+                            background: rgba(0,0,0,0.1);
+                        }
+
+                        &.none-concated {
+                            margin-top: 15px;
+                        }
+
+                        .message-header {
+                            display: flex;
+                            justify-content: space-between;
+                            margin-bottom: 5px;
+
+                            .message-name {
+                                font-weight: bolder;
+                                overflow: hidden;
+                                text-overflow: ellipsis;
+                                max-width: 150px;
+                                margin-right: 10px;
+                            }
+
+                            .message-date {
+                                margin-top: 2px;
+                                font-size: 13px;
+                                color: gray;
+                            }
+                        }
+
+                        .message-text {
+                            font-size: 14px;
+                            margin-bottom: 8px;
+                        }
+                    }
+                }
+            }
+
+            .chat-messages-unread {
+                position: absolute;
+                bottom: 60px;
+                right: 50px;
+                width: 42px;
+                height: 42px;
+                box-shadow: 0 0 3px rgba(0,0,0, 0.5);
+                line-height: 1;
+                padding-top: 10px;
+                text-align: center;
+                font-weight: bolder;
+                border-radius: 50%;
+                background: white;
+                cursor: pointer;
+
+                &:hover {
+                    background: rgba(0,0,0,0.05);
+                }
+
+                img {
+                    margin-top: 2px;
+                    width: 18px;
+                }
+
+                span {
+                    position: absolute;
+                    top: -5px;
+                    right: -5px;
+                    background: gray;
+                    padding: 5px;
+                    font-size: 12px;
+                    color: white;
+                    line-height: 0.8;
+                    margin-left: 1px;
+                    font-weight: normal;
+                    width: 18px;
+                    height: 18px;
+                    border-radius: 50%;
+                }
+            }
         }
     }
 }
